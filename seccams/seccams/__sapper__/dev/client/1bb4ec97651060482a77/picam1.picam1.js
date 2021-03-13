f2f1c7d7f363e9462ea477f42b017a73448969ba
Charlie Smotherman <porthose.cjsmo.cjsmo@gmail.com>
2021-03-13 03:06:16 -0800

    added last fifty back

---
+++ seccams/seccams/__sapper__/dev/client/1bb4ec97651060482a77/picam1.picam1.js
@@ -0,0 +1,15 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["picam1"],{
+
+/***/ "./src/routes/picam1.svelte":
+/*!**********************************!*\
+  !*** ./src/routes/picam1.svelte ***!
+  \**********************************/
+/*! exports provided: default */
+/***/ (function(module, exports) {
+
+throw new Error("Module build failed (from ./node_modules/svelte-loader/index.js):\nError: ParseError: Unexpected token (11:2)\n 9:       console.log(data.plist)\n10:       PList = data.\n11:     }).catch(err => console.log(err));\n        ^\n12:   })\n13: \n    at preprocess.then.catch.err (/media/pi/HD/SecCams/seccams/seccams/node_modules/svelte-loader/index.js:181:12)");
+
+/***/ })
+
+}]);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiIxYmI0ZWM5NzY1MTA2MDQ4MmE3Ny9waWNhbTEucGljYW0xLmpzIiwic291cmNlUm9vdCI6IiJ9
--- seccams/seccams/src/routes/index.svelte
+++ seccams/seccams/src/routes/index.svelte
@@ -75,53 +75,64 @@
 	<title>SecCams</title>
 </svelte:head>
 
-<svg viewBox="0 -5 300 255" >
+<svg viewBox="0 -5 400 325" >
 	<text x="0" y="18" font-size="2em" fill="black">SecCams</text>
 
 	<line x1="50" y1="25" x2="260" y2="25" style="stroke:rgb(255,0,0);stroke-width:2" />
 
 	<text x="0" y="47" font-size="1em" fill="black" >PC1 Motion</text>
-	<text x="135" y="47" font-size="1em" fill="black" >{pc1LastMovingDate}</text>
+	<!-- <text x="135" y="47" font-size="1em" fill="black" >{pc1LastMovingDate}</text> -->
 	<text x="230" y="47" font-size="1em" fill="pink">{pc1LastMovingTime}</text>
 
-	<text x="0" y="67" font-size="1em" fill="black">1 Last Still</text>
-	<text x="135" y="67" font-size="1em" fill="black" >{pc1LastStillDate}</text>
+	<text x="0" y="67" font-size="1em" fill="black">PC1 Still</text>
+	<!-- <text x="135" y="67" font-size="1em" fill="black" >{pc1LastStillDate}</text> -->
 	<text x="230" y="67" font-size="1em" fill="blue">{pc1LastStillTime}</text>
 
-	<text x="0" y="87" font-size="1em" fill="black" on:click={handlepingPiCam1Click}>1 Ping status</text>
-	<text x="135" y="87" font-size="1em" fill="blue">{pc1status}</text>
+	<!-- <line x1="50" y1="93" x2="260" y2="93" style="stroke:rgb(255,0,0);stroke-width:2" /> -->
 
-	<text x="0" y="107" font-size="1em" fill="black" >2 Last Motion</text>
-	<text x="135" y="107" font-size="1em" fill="black" >{pc2LastMovingDate}</text>
+	<text x="0" y="87" font-size="1em" fill="black" on:click={handlepingPiCam1Click}>PC1 Ping</text>
+	<text x="230" y="87" font-size="1em" fill="blue">{pc1status}</text>
+
+	
+
+	<text x="0" y="107" font-size="1em" fill="black" >PC2 Motion</text>
+	<!-- <text x="135" y="107" font-size="1em" fill="black" >{pc2LastMovingDate}</text> -->
 	<text x="230" y="107" font-size="1em" fill="pink">{pc2LastMovingTime}</text>
 
-	<text x="0" y="127" font-size="1em" fill="black">2 Last Still</text>
-	<text x="135" y="127" font-size="1em" fill="black" >{pc2LastStillDate}</text>
+	<text x="0" y="127" font-size="1em" fill="black">PC2 Still</text>
+	<!-- <text x="135" y="127" font-size="1em" fill="black" >{pc2LastStillDate}</text> -->
 	<text x="230" y="127" font-size="1em" fill="blue">{pc2LastStillTime}</text>
 
-	<text x="0" y="147" font-size="1em" fill="black" on:click={handlepingPiCam2Click}>2 Ping Status</text>
-	<text x="135" y="147" font-size="1em" fill="blue" >{pc2status}</text>
+	<text x="0" y="147" font-size="1em" fill="black" on:click={handlepingPiCam2Click}>PC2 Ping</text>
+	<text x="230" y="147" font-size="1em" fill="blue" >{pc2status}</text>
 
 	<text x="0" y="167" font-size="1em" fill="black">Health Check</text>
-	<text x="135" y="167" font-size="1em" fill="black" >{lhedate}</text>
-	<text x="230" y="167" font-size="1em" fill="red">{lhetime}</text>
-
-	<text x="0" y="187" font-size="1em" fill="black">PiCam1 Total</text>
-	<text x="135" y="187" font-size="1em" fill="black" >{pc1TotalEvents}</text>
+	<text x="230" y="167" font-size="1em" fill="black" >{lhedate}</text>
 	<!-- <text x="230" y="167" font-size="1em" fill="red">{lhetime}</text> -->
 
-	<text x="0" y="207" font-size="1em" fill="black">PiCam2 Total</text>
-	<text x="135" y="207" font-size="1em" fill="black" >{pc2TotalEvents}</text>
+	<text x="0" y="187" font-size="1em" fill="black">PC1 Total</text>
+	<text x="230" y="187" font-size="1em" fill="black" >{pc1TotalEvents}</text>
+	<!-- <text x="230" y="167" font-size="1em" fill="red">{lhetime}</text> -->
+
+	<text x="0" y="207" font-size="1em" fill="black">PC2 Total</text>
+	<text x="230" y="207" font-size="1em" fill="black" >{pc2TotalEvents}</text>
 	<!-- <text x="230" y="167" font-size="1em" fill="red">{lhetime}</text> -->
 
 	<text x="0" y="227" font-size="1em" fill="black">Total Events</text>
-	<text x="135" y="227" font-size="1em" fill="black" >{totalEvents}</text>
+	<text x="230" y="227" font-size="1em" fill="black" >{totalEvents}</text>
 	<!-- <text x="230" y="167" font-size="1em" fill="red">{lhetime}</text> -->
 
 	<text x="0" y="247" font-size="1em" fill="black">Pic Folder Size</text>
-	<text x="135" y="247" font-size="1em" fill="black">{picdirsize}</text>
+	<text x="230" y="247" font-size="1em" fill="black">{picdirsize}</text>
 
+	<text x="0" y="267" font-size="1em" fill="black">Garage Door Status</text>
+	<text x="230" y="267" font-size="1em" fill="black">Closed</text>
 
+	<text x="0" y="287" font-size="1em" fill="black">Grandma Status</text>
+	<text x="230" y="287" font-size="1em" fill="black">Not Home</text>
+
+	<text x="0" y="307" font-size="1em" fill="black">People Status</text>
+	<text x="230" y="307" font-size="1em" fill="black">None</text>
 
 	Sorry, your browser does not support inline SVG.  
 </svg>
